55ad36980adc7bbd552ef5b08451fda4b2722467
APIN <lerooooooooooooooy@gmail.com>
2018-11-10 19:37:04 -0500

    Initial commit, new react app and json data.

---
+++ src/json.js
@@ -0,0 +1,151 @@
+{  
+  game: {
+    // Game
+    id: 1,
+    date: 0,
+    time: 0,
+    teamOneStart: true,
+    tournament: ?,
+    rosters: [
+      {
+        [
+    	    1: {
+    	      name: 'P1N',
+    	      gender: 'M',
+    	    },
+    	    2: {
+    	      name: 'P2N',
+    	      gender: 'F',
+    	    },
+    	    3: {
+    	      name: 'P3N',
+    	      gender: 'M',
+    	    },
+    	    4: {
+    	      name: 'P4N',
+    	      gender: 'F',
+    	    },
+    	    5: {
+    	      name: 'P5N',
+    	      gender: 'M',
+    	    },
+    	    6: {
+    	      name: 'P6N',
+    	      gender: 'M',
+    	    },
+    	    7: {
+    	      name: 'P7N',
+    	      gender: 'F',
+    	    },
+        ],
+        // Other team
+        [],
+      }
+    ],
+    lines: [
+      {
+  
+      },
+    ],
+  },
+  
+  team: {
+    // Team
+    id: 1,
+    year: 0,
+    competition_level: 'Club',
+    gender_division: 'Mixed',
+    players: [
+    /* id to UUID */
+      1: {
+        name: 'P1N',
+        gender: 'M',
+      },
+      2: {
+        name: 'P2N',
+        gender: 'F',
+      },
+      3: {
+        name: 'P3N',
+        gender: 'M',
+      },
+      4: {
+        name: 'P4N',
+        gender: 'F',
+      },
+      5: {
+        name: 'P5N',
+        gender: 'M',
+      },
+      6: {
+        name: 'P6N',
+        gender: 'M',
+      },
+      7: {
+        name: 'P7N',
+        gender: 'F',
+      },
+    ],
+    offenses: [
+      {
+      /* id to UUID */
+        id: 1,
+        name: 'Ho Stack',
+        positions: [
+          {
+            name: 'Handler',
+            players: [], /* ??? */
+          },
+          {
+            name: 'Handler',
+          },
+          {
+            name: 'Handler',
+          },
+          {
+            name: 'Cutter',
+          },
+          {
+            name: 'Cutter',
+          },
+          {
+            name: 'Cutter',
+          },
+          {
+            name: 'Cutter',
+          },
+        ],
+      },
+    ],
+    defenses: [
+      {
+        id: 2,
+        name: 'Person D',
+        positions: [
+          {
+            name: 'Handler',
+            players: [], /* ??? */
+          },
+          {
+            name: 'Handler',
+          },
+          {
+            name: 'Handler',
+          },
+          {
+            name: 'Cutter',
+          },
+          {
+            name: 'Cutter',
+          },
+          {
+            name: 'Cutter',
+          },
+          {
+            name: 'Cutter',
+          },
+        ],
+      },
+    ],
+  },
+}
